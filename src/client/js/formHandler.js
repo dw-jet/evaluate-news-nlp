9adfa5cd796e3865272482c05ab867201009606c
James Teitsworth <63876458+dw-jet@users.noreply.github.com>
2020-06-03 10:35:14 -0500

    feat: Form validation and feedback

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,11 +1,24 @@
 function handleSubmit(event) {
   event.preventDefault()
-  const resultDiv = document.getElementById('result');
-  // check what text was put into the form field
-  let formText = document.getElementById('name').value
 
-  Client.postData('http://localhost:3030/sentiment', { url: formText})
-  .then(Client.getHandler('http://localhost:3030/sentiment'))
+  const resultDiv = document.getElementById('results');
+  const inputUrl = document.getElementById('article-url');
+  const formText = inputUrl.value;
+  const inputValid = inputUrl.checkValidity();
+
+  resultDiv.classList.remove('invalid-text');
+
+  inputUrl.value = "";
+  resultDiv.innerHTML = "";
+
+  if (inputValid) {
+    Client.postData('http://localhost:3030/sentiment', { url: formText})
+    .then(Client.getHandler('http://localhost:3030/sentiment'))
+  } else {
+    const resultDiv = document.getElementById('results');
+    resultDiv.classList.add('invalid-text');
+    resultDiv.innerHTML = `<p>${formText} isn't a valid URL. Try to copy and paste the whole URL starting with http:// or https://</p>`;
+  }
 }
 
 export { handleSubmit }
