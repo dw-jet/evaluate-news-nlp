6f5f1f4308438397e4bf8a702d2f6d6e1d771c01
James Teitsworth <james@jamesteitsworth.com>
2020-06-03 18:44:35 -0500

    test: Test DOM manipulation when inserting data

---
+++ __tests__/insertResultsHandler.spec.js
@@ -0,0 +1,8 @@
+import { insertResults } from '../src/client/js/insertResults'
+const data = {polarity: "positive", polarity_confidence: 0.88}
+
+test('dom nodes are updated as expected', () => {
+  document.body.innerHTML = '<div id="results"></div>';
+  insertResults(data);
+  expect(document.getElementById('results').innerHTML).toBe(`<p>Polarity: ${data.polarity}</p> <p>Confidence: ${data.polarity_confidence.toFixed(2)}</p>`)
+})
