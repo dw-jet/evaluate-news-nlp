24d6b1d97748ad25047117f8545e615ea20f697a
James Teitsworth <james@jamesteitsworth.com>
2020-06-04 20:23:36 -0500

    feat: Set up service workers

--- src/server/index.js
+++ src/server/index.js
@@ -28,8 +28,8 @@ const sentimentAnalysis = (url) => {
 }
 
 app.get('/', function (req, res) {
-  // res.sendFile('dist/index.html')
-  res.sendFile(path.resolve('src/client/views/index.html'))
+  res.sendFile('dist/index.html')
+  // res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
