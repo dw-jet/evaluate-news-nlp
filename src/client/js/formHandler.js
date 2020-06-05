2bc37239e5497cc5f2485268b73c743985c9908a
James Teitsworth <james@jamesteitsworth.com>
2020-06-04 21:22:57 -0500

    fix: Prevent invalid style on form before entry

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,11 +1,13 @@
 function handleSubmit(event) {
   event.preventDefault()
 
+  const urlInput = document.getElementById('article-url');
   const resultDiv = document.getElementById('results');
   const inputUrl = document.getElementById('article-url');
   const formText = inputUrl.value;
   const inputValid = inputUrl.checkValidity();
 
+  urlInput.classList.remove('invalid-input');
   resultDiv.classList.remove('invalid-text');
 
   inputUrl.value = "";
@@ -16,6 +18,7 @@ function handleSubmit(event) {
     .then(Client.getHandler('http://localhost:3030/sentiment'))
   } else {
     const resultDiv = document.getElementById('results');
+    urlInput.classList.add('invalid-input');
     resultDiv.classList.add('invalid-text');
     resultDiv.innerHTML = `<p>${formText} isn't a valid URL. Try to copy and paste the whole URL starting with http:// or https://</p>`;
   }
